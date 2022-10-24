b414cd484e3807d57cb50fcf3492531cceddac28
Wushengyouhan <840835067@qq.com>
2022-10-24 23:30:08 +0800

    界面布局

---
+++ src/components/SetupScript.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useStore } from 'vuex';
+const store = useStore()
+const count = computed(()=>{
+    return store.state.count
+})
+
+const increment = ()=> {
+    store.commit('increment')
+}
+</script>
+
+<template>
+<button @click="increment">{{count}}</button>
+<el-button>elementPlus</el-button>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/layout/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="layout">
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss">
+.layout {
+    display: flex;
+    height: 100vh;
+    
+    .el-header {
+        background-color: #b3c0d1;
+        color: var(--el-text-color-primary);
+        margin: 0;
+    }
+
+    .el-aside {
+        background-color: #d3dce6;
+        color: var(--el-text-color-primary);
+        margin: 0;
+    }
+
+    .el-main {
+        background-color: #e9eef3;
+    }
+}
+</style>
